58a89f49bfd7b1c7976de07321884e1f9c4c7f2e
Junior <jose@vert.se>
2020-11-26 21:21:15 -0300

    add testes e novas implementações: search e productcard

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -4,6 +4,25 @@ import { makeServer } from '@/miragejs/server'
 
 describe('ProductCard - unit', () => {
   let server
+
+  const mountProductCard = () => {
+    const product = server.create('product', {
+      title: 'Relógio bonito',
+      price: '23.00',
+      image:
+        'https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    })
+
+    return {
+      wrapper: mount(ProductCard, {
+        propsData: {
+          product,
+        },
+      }),
+      product,
+    }
+  }
+
   beforeEach(() => {
     server = makeServer({ environment: 'test' })
   })
@@ -13,30 +32,24 @@ describe('ProductCard - unit', () => {
   })
 
   it('should match the snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '23.00',
-        }),
-      },
-    })
-
+    const { wrapper } = mountProductCard()
     expect(wrapper.element).toMatchSnapshot()
   })
 
   it('should mount the component', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '23.00',
-        }),
-      },
-    })
+    const { wrapper } = mountProductCard()
 
     expect(wrapper.vm).toBeDefined()
     expect(wrapper.text()).toContain('Relógio bonito')
     expect(wrapper.text()).toContain('23.00')
+  })
+
+  it('should emit the event addToCart with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard()
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.emitted().addToCart).toBeTruthy()
+    expect(wrapper.emitted().addToCart.length).toBe(1)
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }])
   })
 })
